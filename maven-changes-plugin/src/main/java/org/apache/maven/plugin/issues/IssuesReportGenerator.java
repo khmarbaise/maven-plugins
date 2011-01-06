khmarbaise/maven-plugins

commit 24322b0148898f6146d648fb9cfff8d0536b0951

Refactoring.
git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@1056049 13f79535-47bb-0310-9956-ffa450edef68

maven-changes-plugin/src/main/java/org/apache/maven/plugin/issues/IssuesReportGenerator.java
@@ -274,8 +274,10 @@ public class IssuesReportGenerator
     {
         sink.head();
 
+        String title = null;
+        title = bundle.getString( "report.changes.header" );
         sink.title();
-        sink.text( bundle.getString( "report.issues.header" ) );
+        sink.text( title );
         sink.title_();
 
         sink.head_();
@@ -284,7 +286,23 @@ public class IssuesReportGenerator
 
         sink.section1();
 
-        sinkSectionTitle1( sink, bundle.getString( "report.issues.header" ) );
+        sinkSectionTitle1( sink, title );
+    }
+
+    private void sinkCell( Sink sink, String text )
+    {
+        sink.tableCell();
+
+        if ( text != null )
+        {
+            sink.text( text );
+        }
+        else
+        {
+            sink.rawText( "&nbsp;" );
+        }
+
+        sink.tableCell_();
     }
 
     private void sinkEndReport( Sink sink )
@@ -314,22 +332,6 @@ public class IssuesReportGenerator
         sink.text( header );
 
         sink.tableHeaderCell_();
-    }
-
-    private void sinkCell( Sink sink, String text )
-    {
-        sink.tableCell();
-
-        if ( text != null )
-        {
-            sink.text( text );
-        }
-        else
-        {
-            sink.rawText( "&nbsp;" );
-        }
-
-        sink.tableCell_();
     }
 
     private void sinkSectionTitle1( Sink sink, String text )

maven-changes-plugin/src/main/java/org/apache/maven/plugin/changes/ChangesReportGenerator.java
@@ -414,6 +414,7 @@ public class ChangesReportGenerator
     private void sinkBeginReport( Sink sink, ResourceBundle bundle )
     {
         sink.head();
+
         String title = null;
         if ( report.getTitle() != null )
         {
@@ -472,7 +473,7 @@ public class ChangesReportGenerator
         sink.close();
     }
 
-    private void sinkFigure( String image, Sink sink, String altText )
+    private void sinkFigure( Sink sink, String image, String altText )
     {
         sink.figure();
 
@@ -508,7 +509,9 @@ public class ChangesReportGenerator
     private void sinkSectionTitle1Anchor( Sink sink, String text, String anchor )
     {
         sink.sectionTitle1();
+
         sink.text( text );
+
         sink.sectionTitle1_();
 
         sink.anchor( HtmlTools.encodeId( anchor ) );
@@ -558,7 +561,7 @@ public class ChangesReportGenerator
 
         sink.tableCell();
 
-        sinkFigure( image, sink, altText );
+        sinkFigure( sink, image, altText );
 
         sink.tableCell_();
     }

commit 87dc5f07903471ebcd517d8fd8516e789b4d6ffb

o Fix problem introduced in r1056049.
git-svn-id: https://svn.apache.org/repos/asf/maven/plugins/trunk@1056067 13f79535-47bb-0310-9956-ffa450edef68

maven-changes-plugin/src/main/java/org/apache/maven/plugin/issues/IssuesReportGenerator.java
@@ -276,7 +276,7 @@ public class IssuesReportGenerator
         sink.head();
 
         String title = null;
-        title = bundle.getString( "report.changes.header" );
+        title = bundle.getString( "report.issues.header" );
         sink.title();
         sink.text( title );
         sink.title_();